b1f96a4e4f800d287839348f3401a32cf33c1cde
HankHint <hankhint@gmail.com>
2022-01-14 18:50:00 -0600

    added questions and error tested

--- index.js
+++ index.js
@@ -1,147 +1,144 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-
   //
-
+//title prompt for user input
   {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project? (Required)',
-    validate: nameInput => {
-      if (nameInput) {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
         return true;
       } else {
-        console.log('Please enter your project name!');
+        console.log("Please enter your project title!");
         return false;
       }
-    }
-  }
-//description prompt for user input
+    },
+  },
+  //description prompt for user input
   {
-    type: 'input',
-    name: 'description',
-    message: 'Provide a description of the project (Required)',
-    validate: descriptionInput => {
+    type: "input",
+    name: "description",
+    message: "Provide a description of the project (Required)",
+    validate: (descriptionInput) => {
       if (descriptionInput) {
         return true;
       } else {
-        console.log('You need to enter a project description!');
+        console.log("You need to enter a project description!");
         return false;
       }
-    }
+    },
   },
 
   //installation prompt
   {
-    type: 'input',
-    name: 'installation',
-    message: 'Provide a description of how to install the the project (Required)',
-    validate: installationInput => {
+    type: "input",
+    name: "installation",
+    message:
+      "Provide a description of how to install the the project (Required)",
+    validate: (installationInput) => {
       if (installationInput) {
         return true;
       } else {
-        console.log('You need to enter installation guidelines!');
+        console.log("You need to enter installation guidelines!");
         return false;
       }
-    }
+    },
   },
-  
+
   //usage prompt
   {
-    type: 'input',
-    name: 'usage',
-    message: 'Provide a description of how to use the project (Required)',
-    validate: usageInput => {
+    type: "input",
+    name: "usage",
+    message: "Provide a description of how to use the project (Required)",
+    validate: (usageInput) => {
       if (usageInput) {
         return true;
       } else {
-        console.log('You need to enter a project description of how to use the project!');
+        console.log(
+          "You need to enter a project description of how to use the project!"
+        );
         return false;
       }
-    }
+    },
   },
 
   //contributing prompt
   {
-    type: 'input',
-    name: 'contribution',
-    message: 'Provide a description of the contributions to the project (Required)',
-    validate: contributionInput => {
+    type: "input",
+    name: "contribution",
+    message:
+      "Provide a description of the contributions to the project (Required)",
+    validate: (contributionInput) => {
       if (contributionInput) {
         return true;
       } else {
-        console.log('You need to enter a project description!');
+        console.log("You need to enter a project description!");
         return false;
       }
-    }
+    },
   },
 
-  //Tests prompt
+  //Test prompt
   {
-    type: 'input',
-    name: 'test',
-    message: 'Provide tests for the project (Required)',
-    validate: testInput => {
+    type: "input",
+    name: "test",
+    message: "Provide tests for the project (Required)",
+    validate: (testInput) => {
       if (testInput) {
         return true;
       } else {
-        console.log('You need to enter a project description!');
+        console.log("You need to enter a project description!");
         return false;
       }
-    }
+    },
   },
 
   //license questions
   {
-    type: 'checkbox',
-    name: 'license',
-    message: 'Which licenese will apply to this project? (Check ONLY one)',
-    choices: ['Apache License 2.0', 'GNU GPLv3', 'GNU GPLv3', 'MIT','ISC', ],
-   
-    //ensures only one license is selected
-    validate: licenseInput => {
-      if (licenseInput[1]) {
-return true; 
-    } else {
-      console.log('Please choose only one license.');
-    }
-  
-    //github username prompt
-  }
-},
+    type: "list",
+    name: "license",
+    message: "Which licenese will apply to this project? (Check ONLY one)",
+    choices: ["Apache License 2.0", "GNU GPLv2", "GNU GPLv3", "MIT", "ISC"],
 
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username (Required)',
-        validate: githubInput => {
-          if (githubInput) {
-            return true;
-          } else {
-            console.log('Please enter your GitHub username!');
-            return false;
-          }
-        }
+
+
+    
+  },
+  //github username prompt
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username (Required)",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter your GitHub username!");
+        return false;
       }
+    },
+  },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+// var mypromise = new Promise(function (resolve, reject){ })
+// //mypromise.catch
+// console.log(mypromise)
 
 inquirer
-  .prompt([
-    questions
-  ])
+  .prompt(questions)
   .then((answers) => {
     // Use user feedback for... whatever!!
     console.log(answers.why);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  
+  //apache license badge
+  if (license === "Apache License 2.0") {
+    return  `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  }
+
+//gpl 2 badge
+  if (license === 'GNU GPLv2') {
+    return '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+    
+  }
+  if (license === 'GNU GPLv3') {
+    return '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+  
+  }
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+ 
+  }
+  if (license === 'ISC') {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+    
+  }
 
   if (!license) {
     return '';
@@ -9,6 +32,18 @@ function renderLicenseBadge(license) {
 
   }
 }
+
+//choices: ["Apache License 2.0", "GNU GPLv2", "GNU GPLv3", "MIT", "ISC"],
+
+// [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+// [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+
+// [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+// [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -31,6 +66,8 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${ renderLicenseBadge(data.license)}
 
   # ${data.description}
 
